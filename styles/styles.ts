2b84af653a251156db2028b71a07e2a951e9f533
Hoangtran135 <hoangtranxuan04@gmail.com>
2025-06-10 21:59:20 +0700

    add

---
+++ styles/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 8,
+    marginVertical: 10,
+  },
+});
